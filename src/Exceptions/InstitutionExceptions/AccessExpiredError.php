7fb946a77d91b73c30408409f9358e7bf00b829a
SharKom <34657941+SharKom@users.noreply.github.com>
2022-10-01 00:13:14 +0200

    Update AccessExpiredError.php

--- src/Exceptions/InstitutionExceptions/AccessExpiredError.php
+++ src/Exceptions/InstitutionExceptions/AccessExpiredError.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Nordigen\NordigenPHP\Exceptions\InstitutionExceptions;
+namespace sharkom\NordigenPHP\Exceptions\InstitutionExceptions;
 
 /**
  * Access to the account has expired or it has been revoked. To restore access reconnect the account.
